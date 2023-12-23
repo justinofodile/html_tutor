fd129440afd781cf7c4685e0a38f808c78fa4021
Ofodile Justin <justinofodile@gmail.com>
2023-12-23 12:44:51 +0100

    First Commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let firstNumber = document.getElementById('first-number')
+let secondNumber = document.getElementById('second-number')
+let btnCalculate = document.getElementById('btn-calculate')
+// let secondNumber = "Calculate Everything again"
+
+
+const handleClick = () => {
+    // console.log(firstNumber + secondNumber)
+    alert(34 + 80)
+}
+
+
+btnCalculate.addEventListener('click', handleClick)
